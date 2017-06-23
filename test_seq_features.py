8e94bfcec879a97b1a758b823e44ab5685fd2dc7
paulinedurand <pdurand@caltech.edu>
2017-06-23 11:33:31 -0700

    Initial commit for the seqfeature

---
+++ test_seq_features.py
@@ -0,0 +1,30 @@
+import test_drive_dev as sf
+import pytest
+
+
+
+def test_n_neg_for_single_E_or_D():
+    """Perform unit tests on n_neg."""
+
+    assert sf.n_neg('E') == 1
+    assert sf.n_neg('D') == 1
+
+def test_n_neg_for_empty_sequence():
+    assert sf.n_neg('') == 0
+
+def test_n_neg_for_longer_sequences():
+    assert sf.n_neg('ACKLWTTAE') == 1
+    assert sf.n_neg('DDDDEEEE') == 8
+
+def test_n_neg_for_lower_case_sequences():
+    assert sf.n_neg('acklwttae') == 1
+
+
+def test_n_neg_for_invalid_amino_acid():
+    with pytest.raises(RuntimeError) as excinfo:
+        sf.n_neg('Z')
+    excinfo.match("Z is not a valid amino acid")
+
+    with pytest.raises(RuntimeError) as excinfo:
+        sf.n_neg('0')
+    excinfo.match("0 is not a valid amino acid")
